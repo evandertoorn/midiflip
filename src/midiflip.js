025a8d549f304efb342db5711fd072ba42ccc637
evandertoorn <eleazvandertoorn@hotmail.com>
2023-02-14 09:59:48 +0100

    Update midiflip.js

--- src/midiflip.js
+++ src/midiflip.js
@@ -21,7 +21,7 @@
 		
 		var midiFile = new MIDIFile(arrayBuffer);
 		
-		for(var track_index = 0; track_index < midiFile.tracks.length; track_index++){
+		for(var track_index = 0; track_index < 1; track_index++){
 			console.log("track_index: ", track_index)
 			var tntmwp = false;
 			var events = midiFile.getTrackEvents(track_index);
